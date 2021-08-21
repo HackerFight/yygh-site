2ea80a3c9311bf4417b7ef29e6a998b8d62f96bc
qiuguan <18368116334@163.com>
2021-08-21 20:01:52 +0800

    init

---
+++ api/hosp.js
@@ -0,0 +1,40 @@
+import request from '@/utils/request'
+
+const api_name = `/api/hosp/hospital`
+
+
+export default {
+
+    getHospitalList(page, limit, searchObj) {
+        return request({
+            url: `${api_name}/findHospList/${page}/${limit}`,
+            method: 'get',
+            params: searchObj   //不是json，后端没有使用 @RequestBody注解
+        })
+    },
+
+    getHospByName(hosname) {
+        return request({
+            url: `${api_name}/findHospByName/${hosname}`,
+            method: 'get'
+        })
+    },
+
+    //根据医院编号获取医院预约挂号详情
+    show(hoscode) {
+        return request({
+            url: `${api_name}/findHospDetail/${hoscode}`,
+            method: 'get'
+        })
+    },
+    
+    //根据医院编号获取科室信息
+    findDepartment(hoscode) {
+        return request({
+            url: `${api_name}/department/${hoscode}`,
+            method: 'get'
+        })
+    }
+
+
+}
---
+++ pages/hospital/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="home page-component">test 固定路由</div>
+</template>      
