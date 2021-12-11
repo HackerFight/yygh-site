256b79634d5c00750e6fd8fa9d479b03bbe85ad9
qiuguan <18368116334@163.com>
2021-12-11 22:15:41 +0800

    订单列表

--- api/order.js
+++ api/order.js
@@ -17,5 +17,22 @@ export default {
             url: `${api_name}/auth/getOrders/${orderId}`,
             method: `get`
         })
-     }
+     },
+
+    //订单列表
+    getPageList(page, limit, searchObj) {
+        return request({
+          url: `${api_name}/auth/list/${page}/${limit}`,
+          method: 'get',
+          params: searchObj
+        })
+    },
+
+    //订单状态
+    getStatusList() {
+        return request({
+          url: `${api_name}/auth/getStatusList`,
+          method: 'get'
+        })
+    }
 }
